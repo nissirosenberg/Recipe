e179aacc4210d0b0eb3bf66c408a34a749ec73f4
nissirosenberg <nissirosenberg@gmail.com>
2023-11-07 19:02:41 -0500

    fixedHW

--- HeartyHearthDB/2 Functions/RecipeSpecifics.sql
+++ HeartyHearthDB/2 Functions/RecipeSpecifics.sql
@@ -3,17 +3,28 @@ returns varchar(1000)
 as
 begin
 	declare @value varchar(1000) = ''
+
+	select @value = 
+					concat(r.RecipeName, 
+					' (', 
+					ct.CuisineTypeName, 
+					') has ', 
+					count(distinct ri.RecipeIngredientsId), 
+					' ingredients and ',
+					count(distinct rd.RecipeDirectionsId),
+					' steps.'
+					)
+	from Recipe r 
+	join CuisineType ct 
+	on r.CuisineTypeId = ct.CuisineTypeId
+	join RecipeIngredients ri
+	on r.RecipeId = ri.RecipeId
+	join RecipeDirections rd
+	on r.RecipeId = rd.RecipeId
+	where r.RecipeId = @RecipeId
+	group by r.RecipeName, ct.CuisineTypeName
 	
-	select @value = concat(
-	(select r.RecipeName from Recipe r where r.RecipeId = @RecipeId),
-	' (',
-	(select ct.CuisineTypeName from Recipe r join CuisineType ct on r.CuisineTypeId = ct.CuisineTypeId where r.RecipeId = @RecipeId),
-	') has ',
-	(select count(ri.RecipeIngredientsId) from Recipe r join RecipeIngredients ri on r.RecipeId = ri.RecipeId where r.RecipeId = @RecipeId),
-	' ingredients and ',
-	(select count(rd.RecipeDirectionsId) from Recipe r join RecipeDirections rd on r.RecipeId = rd.RecipeId where r.RecipeId = @RecipeId),
-	' steps.'
-	) 
+
 
 
 
@@ -21,4 +32,4 @@ begin
 end
 go
 
-select dbo.RecipeSpecifics(r.RecipeId), * from Recipe r 
+select dbo.RecipeSpecifics(r.RecipeId) from Recipe r 
