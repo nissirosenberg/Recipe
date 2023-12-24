9a7ad37778eab8c6140f33b8395bbf92fc59da22
nissirosenberg <nissirosenberg@gmail.com>
2023-12-23 19:10:31 -0500

    copied bizIngredient

--- RecipesApps/RecipeSystem/bizIngredient.cs
+++ RecipesApps/RecipeSystem/bizIngredient.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RecipeSystem
+﻿namespace RecipeSystem
 {
-    internal class bizIngredient
+    public class bizIngredient : bizObject<bizIngredient>
     {
+        private int _ingrdientid;
+        private string _ingredientname;
+        private string _ingredientpicture;
+
+        public List<bizIngredient> Search(string ingredientnameval)
+        {
+            SqlCommand cmd = SQLUtility.GetSQLCommand(this.GetSprocName);
+            SQLUtility.SetParameterValue(cmd, "IngredientName", ingredientnameval);
+            DataTable dt = SQLUtility.GetDataTable(cmd);
+            return this.GetListFromDataTable(dt);
+        }
+
+        public int IngredientId
+        {
+            get => _ingrdientid;
+            set
+            {
+                if (value != _ingrdientid)
+                {
+                    _ingrdientid = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        public string IngredientName
+        {
+            get => _ingredientname;
+            set
+            {
+                if (value != _ingredientname)
+                {
+                    _ingredientname = value;
+                    InvokePropertyChanged();
+                    InvokePropertyChanged("IngredientPicture");
+                }
+            }
+        }
+
+        public string IngredientPicture
+        {
+            get => _ingredientpicture;
+        }
     }
 }
